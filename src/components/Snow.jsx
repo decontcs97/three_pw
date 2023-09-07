c7965bc225b22c6c3545bddb1cc5548805b41f3f
decontcs97 <83840531+decontcs97@users.noreply.github.com>
2023-09-07 19:23:29 -0400

    video rendering added

--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -1,116 +1,78 @@
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
-import sColor from '../assets/Snow_004_COLOR.jpg'
-import sNorm from '../assets/Snow_004_NORM.jpg'
-import sDisp from '../assets/Snow_004_DISP.png'
-import sRough from '../assets/Snow_004_ROUGH.jpg'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const Snow = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
     let scene, camera, renderer;
-    const planeW = 200;
-    const planeH = 200;
+    let time = 0.0;
 
-    // Set up the scene
     scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+
+
+    //camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.001, 1000);
+    camera = new THREE.OrthographicCamera(1.4 * 1 / - 2, 1.4 * 1 / 2, 1.4 / 2, 1.4 / -2, -1000, 1000);
+    camera.position.set(0,0,2);
+
+
+
     renderer = new THREE.WebGLRenderer({ antialias: true, canvas: canvasRef.current });
     renderer.setSize(window.innerWidth, window.innerHeight);
-
-    // Add ambient light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-    scene.add(ambientLight);
-
-    // Add directional light
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-    directionalLight.position.set(1, 1, 1);
-    scene.add(directionalLight);
-
-    // Load snow texture for plane
-    const textureLoader = new THREE.TextureLoader();
-    const sCol = textureLoader.load(sColor);
-    const sNor = textureLoader.load(sNorm);
-    const sDis = textureLoader.load(sDisp);
-    const sRo = textureLoader.load(sRough);
-
-    // Create the flat plane
-    const planeGeometry = new THREE.PlaneGeometry(planeW, planeH, 1024, 1024);
-    const snowMaterial = new THREE.MeshStandardMaterial({
-      map: sCol,
-      normalMap: sNor,
-      displacementMap: sDis,
-      displacementScale: 5.5,
-      roughnessMap: sRo
+    let controls = new OrbitControls(camera, renderer.domElement);
+    let material = new THREE.ShaderMaterial({
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { type: "f", value: 0 },
+        resolution: { type: "v4", value: new THREE.Vector4() },
+        uvRate1: {
+          value: new THREE.Vector2(1,1)
+        }
+      },
+        vertexShader: `
+        varying vec2 vUv;
+        varying vec3 vPosition;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * instanceMatrix * vec4(position.x, position.y, position.z, 1.0); 
+        }
+        `,
+        fragmentShader: `
+        varying vec2 vUv;
+        varying vec3 vPosition;
+        void main() {
+          gl_FragColor = vec4(vUv,0.0,1.0);
+        }
+        `,
     });
 
-    //EDIT: MAKE A LOOP TO DO THIS...JUST LAZY RIGHT NOW
-    const planeMesh = new THREE.Mesh(planeGeometry, snowMaterial);
-    const planeMesh2 = new THREE.Mesh(planeGeometry, snowMaterial); 
-    const planeMesh3 = new THREE.Mesh(planeGeometry, snowMaterial);
-    const planeMesh4 = new THREE.Mesh(planeGeometry, snowMaterial);
-    const planeMesh5 = new THREE.Mesh(planeGeometry, snowMaterial);
-    const planeMesh6 = new THREE.Mesh(planeGeometry, snowMaterial);
-    
-    scene.add(planeMesh);
-    planeMesh.rotation.x = -Math.PI / 2;
-    planeMesh.position.set(0,0,0);
-
-    scene.add(planeMesh3);
-    planeMesh3.rotation.x = -Math.PI / 2;
-    planeMesh3.position.set(0,0,planeW - 1);
+    let gridSize = 1;
+    let size = 50;
+    let cellSize = gridSize / size;
 
 
-    // Create the starfield geometry
-    const starCount = 1000;
-    const starFieldGeometry = new THREE.BufferGeometry();
-    const starFieldMaterial = new THREE.PointsMaterial({
-      color: 0xffffff,
-      size: 1,
-      transparent: true,
-      blending: THREE.AdditiveBlending,
-      depthWrite: false 
-    });
+    let geometry = new THREE.PlaneGeometry(cellSize,cellSize);
 
-    const starPositions = new Float32Array(starCount * 3);
-    const starVelocities = new Float32Array(starCount * 3);
-    for (let i = 0; i < starCount; i++) {
-      const i3 = i * 3;
-      starPositions[i3] = Math.random() * 2000 - 1000; // x 
-      starPositions[i3 + 1] = Math.random() * 2000 - 1000; // y 
-      starPositions[i3 + 2] = Math.random() * 2000 - 1000; // z 
-
-      starVelocities[i3] = -Math.random() * 0.5; // x velocity
-      starVelocities[i3 + 1] = -Math.random() * 0.5; // y velocity
-      starVelocities[i3 + 2] = Math.random() * 0.2 - 0.1; // z velocity
+    let plane = new THREE.InstancedMesh(geometry, material, size**2);
+    let dummy = new THREE.Object3D();
+    let count = 0;
+    for(let i = 0; i < size; i++) {
+      for(let j = 0; j < size; j++) {
+        dummy.position.set(i*cellSize-0.5, j*cellSize-0.5);
+        dummy.updateMatrix();
+        plane.setMatrixAt(count++, dummy.matrix);
+      }
     }
+    plane.instanceMatrix.needsUpdate = true;
+    plane.geometry
 
-    starFieldGeometry.setAttribute('position', new THREE.BufferAttribute(starPositions, 3));
-    const starFieldPoints = new THREE.Points(starFieldGeometry, starFieldMaterial);
-    scene.add(starFieldPoints);
-
-    // Adjust camera position and look at the plane
-    camera.position.set(0, 100, 300);
-    camera.lookAt(planeMesh.position);
-
+    scene.add(plane);
     // Animation loop
     const animate = () => {
+      time += 0.05;
       requestAnimationFrame(animate);
-
-      // Update star positions
-      const positions = starFieldGeometry.attributes.position.array;
-      for (let i = 0; i < starCount; i++) {
-        const i3 = i * 3;
-        positions[i3] += starVelocities[i3];
-        positions[i3 + 1] += starVelocities[i3 + 1];
-        if (positions[i3] < -1000 || positions[i3 + 1] < -1000) {
-          positions[i3] = 1000;
-          positions[i3 + 1] = 1000;
-        }
-      }
-      starFieldGeometry.attributes.position.needsUpdate = true;
-
       renderer.render(scene, camera);
     };
 
@@ -129,10 +91,6 @@ const Snow = () => {
     // Clean up the scene on component unmount
     return () => {
       window.removeEventListener('resize', handleResize);
-      scene.remove(starFieldPoints);
-      scene.remove(planeMesh);
-      scene.remove(ambientLight);
-      scene.remove(directionalLight);
       renderer.dispose();
 
     };
@@ -140,6 +98,5 @@ const Snow = () => {
 
   return <canvas ref={canvasRef} style={{ position: 'fixed', top: 0, left: 0, zIndex: -1 }} />;
 };
-
 
 export default Snow;
